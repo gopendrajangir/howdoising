4f36e7be4f85978ee39ac3aaccd6445b978fe960
gopendrajangir <gopendra1.kumar@gmail.com>
2019-06-22 04:46:34 +0530

    commit

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,12 +1,11 @@
 const User = require('./../../models/User');
 const mongoose = require('./../../bin/www').mongoose;
+const ObjectId = mongoose.Types.ObjectId;
 const db = mongoose.connection.db;
 
 module.exports = (req, res) => {
   const sessionID = req.sessionID;
   const userId = req.user;
-
-  console.log(req.user);
 
   User.findOne({ _id: mongoose.Types.ObjectId(userId)}, (err, user) => {
     if(err) {
@@ -27,19 +26,22 @@ module.exports = (req, res) => {
         return res.status(500).send("Internal Server Error");
       }
       if(!session) {
-        res.status(404).json({
+        returnres.status(404).json({
           msg: "Session not found"
         });
       }
       
       const sessions = user.sessions;
       const filteredSessions = sessions.filter(userSession => {
-        if(userSession.$id !== session._id) {
+        if(ObjectId(userSession.oid).equals(ObjectId(session._id))) {
+          console.log("matched");
+          return false;
+        } else {
           return true;
         }
       });
 
-      User.findOneAndUpdate({ id: mongoose.Types.ObjectId(userId)}, { sessions: filteredSessions }, (err, user) => {
+      User.findOneAndUpdate({ _id: mongoose.Types.ObjectId(userId)}, { sessions: filteredSessions }, (err, user) => {
         req.session.destroy((err) => {
           if(err) {
             console.log("Error in req session destory at logout route", err);
