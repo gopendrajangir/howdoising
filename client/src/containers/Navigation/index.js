7c09793b79d67647bdf10ec0f75cbab66534f386
gopendrajangir <gopendra1.kumar@gmail.com>
2019-06-24 01:38:14 +0530

    navigation ui changes

--- client/src/containers/Navigation/index.js
+++ client/src/containers/Navigation/index.js
@@ -10,8 +10,8 @@ const Navigation = ({ isLoggedIn, user }) => {
   const body = document.getElementsByTagName("body")[0];
 
   body.addEventListener("click", event => {
-    const profileNavLabel = $(".app-navigation-auth-profile-head");
-    const profileNavigation = $(".app-navigation-auth-profile-nav");
+    const profileNavLabel = $(".app-navigation-auth-protected-profile-head");
+    const profileNavigation = $(".app-navigation-auth-protected-profile-nav");
     const profileNavigationInput = $("#app-profile-navigation");
 
     if (profileNavigation) {
@@ -28,9 +28,11 @@ const Navigation = ({ isLoggedIn, user }) => {
       }
     }
 
-    const notificationsNavLabel = $(".app-navigation-auth-notifications-head");
+    const notificationsNavLabel = $(
+      ".app-navigation-auth-protected-notifications-head"
+    );
     const notificationsNavigation = $(
-      ".app-navigation-auth-notifications-list"
+      ".app-navigation-auth-protected-notifications-list"
     );
     const notificationsNavigationInput = $("#app-notifications-navigation");
 
@@ -60,26 +62,29 @@ const Navigation = ({ isLoggedIn, user }) => {
       </div>
       <div className="app-navigation-auth">
         {isLoggedIn ? (
-          <React.Fragment>
-            <span className="app-navigation-auth-sing">
-              <Link className="app-navigation-auth-sing-link" to="/sing">
-                <span className="app-navigation-auth-sing-link-icon">
+          <div className="app-navigation-auth-protected">
+            <span className="app-navigation-auth-protected-sing">
+              <Link
+                className="app-navigation-auth-protected-sing-link"
+                to="/sing"
+              >
+                <span className="app-navigation-auth-protected-sing-link-icon">
                   <svg>
                     <use xlinkHref={`${SvgSprite}#icon-microphone`} />
                   </svg>
                 </span>
-                <span className="app-navigation-auth-sing-link-title">
+                <span className="app-navigation-auth-protected-sing-link-title">
                   Sing
                 </span>
               </Link>
             </span>
-            <span className="app-navigation-auth-notifications">
+            <span className="app-navigation-auth-protected-notifications">
               <label
                 htmlFor="app-notifications-navigation"
-                className="app-navigation-auth-notifications-head"
+                className="app-navigation-auth-protected-notifications-head"
               >
-                <span className="app-navigation-auth-notifications-head-icon">
-                  <span className="app-navigation-auth-notifications-head-icon-number">
+                <span className="app-navigation-auth-protected-notifications-head-icon">
+                  <span className="app-navigation-auth-protected-notifications-head-icon-number">
                     9+
                   </span>
                   <svg>
@@ -88,16 +93,16 @@ const Navigation = ({ isLoggedIn, user }) => {
                 </span>
               </label>
               <input hidden id="app-notifications-navigation" type="checkbox" />
-              <span className="app-navigation-auth-notifications-list">
+              <span className="app-navigation-auth-protected-notifications-list">
                 Empty
               </span>
             </span>
-            <span className="app-navigation-auth-profile">
+            <span className="app-navigation-auth-protected-profile">
               <label
                 htmlFor="app-profile-navigation"
-                className="app-navigation-auth-profile-head"
+                className="app-navigation-auth-protected-profile-head"
               >
-                <span className="app-navigation-auth-profile-head-image">
+                <span className="app-navigation-auth-protected-profile-head-image">
                   <img
                     src={`/apis/images/${user.image.$id}`}
                     alt="user"
@@ -106,64 +111,64 @@ const Navigation = ({ isLoggedIn, user }) => {
                     }}
                   />
                 </span>
-                <span className="app-navigation-auth-profile-head-name">
+                <span className="app-navigation-auth-protected-profile-head-name">
                   {user.displayname}
                 </span>
-                <span className="app-navigation-auth-profile-head-icon">
+                <span className="app-navigation-auth-protected-profile-head-icon">
                   <svg>
                     <use xlinkHref={`${SvgSprite}#icon-cheveron-down`} />
                   </svg>
                 </span>
               </label>
               <input hidden id="app-profile-navigation" type="checkbox" />
-              <span className="app-navigation-auth-profile-nav">
+              <span className="app-navigation-auth-protected-profile-nav">
                 <Link
-                  className="app-navigation-auth-profile-nav-link"
+                  className="app-navigation-auth-protected-profile-nav-link"
                   to="/profile"
                 >
-                  <span className="app-navigation-auth-profile-nav-link-icon">
+                  <span className="app-navigation-auth-protected-profile-nav-link-icon">
                     <svg>
                       <use xlinkHref={`${SvgSprite}#icon-user`} />
                     </svg>
                   </span>
-                  <span className="app-navigation-auth-profile-nav-link-title">
+                  <span className="app-navigation-auth-protected-profile-nav-link-title">
                     Profile
                   </span>
                 </Link>
                 <Link
-                  className="app-navigation-auth-profile-nav-link"
+                  className="app-navigation-auth-protected-profile-nav-link"
                   to="/settings"
                 >
-                  <span className="app-navigation-auth-profile-nav-link-icon">
+                  <span className="app-navigation-auth-protected-profile-nav-link-icon">
                     <svg>
                       <use xlinkHref={`${SvgSprite}#icon-cog`} />
                     </svg>
                   </span>
-                  <span className="app-navigation-auth-profile-nav-link-title">
+                  <span className="app-navigation-auth-protected-profile-nav-link-title">
                     Settings
                   </span>
                 </Link>
                 <Link
-                  className="app-navigation-auth-profile-nav-link"
+                  className="app-navigation-auth-protected-profile-nav-link"
                   to="/logout"
                 >
-                  <span className="app-navigation-auth-profile-nav-link-icon">
+                  <span className="app-navigation-auth-protected-profile-nav-link-icon">
                     <svg>
                       <use xlinkHref={`${SvgSprite}#icon-switch`} />
                     </svg>
                   </span>
-                  <span className="app-navigation-auth-profile-nav-link-title">
+                  <span className="app-navigation-auth-protected-profile-nav-link-title">
                     Logout
                   </span>
                 </Link>
               </span>
             </span>
-          </React.Fragment>
+          </div>
         ) : (
-          <React.Fragment>
+          <div className="app-navigation-auth-public">
             <Link to="/signup">Signup</Link>
             <Link to="/login">Login</Link>
-          </React.Fragment>
+          </div>
         )}
       </div>
     </div>
