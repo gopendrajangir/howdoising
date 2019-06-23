320450a06113fa15b7c3f5621049199dea18f0af
gopendrajangir <gopendra1.kumar@gmail.com>
2019-06-23 13:03:10 +0530

    remote sessions

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,11 +1,6 @@
 module.exports = (req, res) => {
-  req.logout((err) => {
-    if(err) {
-      console.log("Error in req session destory at logout route", err);
-      return res.status(500).send("Internal Server Error");
-    }
-    res.status(200).json({
-      msg: "You logged out successfully"
-    });
+  req.logout();
+  res.status(200).json({
+    msg: "You logged out successfully"
   });
 }
