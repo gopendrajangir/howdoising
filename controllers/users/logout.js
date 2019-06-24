aae2257f41ac0371b081adddbe0da441b958b7bc
gopendrajangir <gopendra1.kumar@gmail.com>
2019-06-25 01:24:22 +0530

    sessions remove ui

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,6 +1,12 @@
 module.exports = (req, res) => {
   req.logout();
-  res.status(200).json({
-    msg: "You logged out successfully"
-  });
+  req.session.destroy((err) => {
+    if(err) {
+      console.log("Error in session destroy at logout route", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    return res.status(200).json({
+      msg: "You logged out successfully"
+    });
+  })
 }
